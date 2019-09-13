b83f626ad17974696b36df64655556509388a8b5
Nick-Hericks <nickhericks@gmail.com>
2019-09-13 12:14:46 +0200

    Set up initial Songkick data fetch

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,45 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+
+	state = {
+		music: []
+	}
+	
+
+	componentDidMount() {
+		fetch('https://api.songkick.com/api/3.0/artists/379603/gigography.json?apikey=WsvDSgM98wiuOncG')
+			.then( response => response.json() )
+			.then( data => {
+				this.setState({ music: data.resultsPage.results.event });
+			})
+			.catch( error =>
+				console.log("Error fetching or parsing data", error)
+			);
+	}
+	
+
+
+
   render() {
+
+		console.log(this.state.music);
+
     return (
-			<div className='page'>
-				Home
+			<div className=''>
+
+				{this.state.music.map( (item, index) => (
+					<div>
+
+						{item.displayName}
+
+					</div>
+				))}
+
+
+
+
+				
 			</div>
 		)
   }
