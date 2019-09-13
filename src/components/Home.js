27431f815212fbfa9fbf9209118bfa01d89177c0
Nick-Hericks <nickhericks@gmail.com>
2019-09-13 12:19:35 +0200

    Add gigogrpahy button

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,14 +8,14 @@ class Home extends Component {
 	
 
 	componentDidMount() {
-		fetch('https://api.songkick.com/api/3.0/artists/379603/gigography.json?apikey=WsvDSgM98wiuOncG')
-			.then( response => response.json() )
-			.then( data => {
-				this.setState({ music: data.resultsPage.results.event });
-			})
-			.catch( error =>
-				console.log("Error fetching or parsing data", error)
-			);
+		fetch(
+      "https://api.songkick.com/api/3.0/artists/395944/gigography.json?apikey=WsvDSgM98wiuOncG"
+    )
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ music: data.resultsPage.results.event });
+      })
+      .catch(error => console.log("Error fetching or parsing data", error));
 	}
 	
 
@@ -27,6 +27,8 @@ class Home extends Component {
 
     return (
 			<div className=''>
+
+				<button>Request gigography</button>
 
 				{this.state.music.map( (item, index) => (
 					<div>
